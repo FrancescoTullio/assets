1d2d2f3b7a5e330f7bf5385a8fea7086f1a6acab
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-13 16:10:15 +0100

    feat:esercizio

--- js/script.js
+++ js/script.js
@@ -36,3 +36,38 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+const sectionMain = document.getElementById("section-card");
+
+const genCard = (member) => {
+  const { name, role, email, img } = member;
+  return `<div class="col-4 col-md-6 col-sm-12">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">${name}</h5>
+                              <p class="card-text">${role}</p>
+                              <a href="${email}" class="card-text">${email}</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                </div>`
+}
+
+
+const renderCart = () => {
+  let cards = "";
+  for (let i = 0; i < teamMembers.length; i++) {
+    const cardForArray = genCard(teamMembers[i]);
+    cards += cardForArray;
+  }
+  sectionMain.innerHTML = cards;
+};
+
+renderCart();
